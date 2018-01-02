47d8bdcd76b00e97ce7d608c4d1010d3e6b62a98
FlagTrone <FlagTrone9852@gmail.com>
2018-01-02 02:29:23 +0100

    page d'acceille v1.3

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
+
+<head>
+  <title>W3.CSS Template</title>
+</head>
+
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-.fa-anchor,.fa-coffee {font-size:200px}
+  body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+  .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+  .fa-anchor,.fa-coffee {font-size:200px}
 </style>
+
 <body>
 
 <!-- Navbar -->
 <div class="w3-top">
   <div class="w3-bar w3-red w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Acueille</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 1</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 2</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Accueil</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">À propos</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Nouveautées</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Documentations</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Catégories</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contacte</a>
   </div>
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">À propos</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Nouveautées</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Documentations</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Catégories</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Contacte</a>
   </div>
 </div>
 
 <!-- Header -->
-<header class="w3-container w3-red w3-center" style="padding:128px 16px">
-  <h1 class="w3-margin w3-jumbo">฿IBLIOTECHA</h1>
-  <p class="w3-xlarge">FIND YOUR R00T.</p>
+<header class="w3-container w3-black w3-center" style="padding:128px 16px">
+  <h1 class="w3-margin w3-text-red w3-jumbo">฿IBLIOTECHA</h1>
+  <p class="w3-xlarge w3-text-green">FIND YOUR R00T.</p>
   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">RECHERCHER</button>
 </header>
 
@@ -46,11 +54,10 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
   <div class="w3-content">
     <div class="w3-twothird">
       <h1>฿ibliotecha, c'est quoi?</h1>
-      <h5 class="w3-padding-32">C'est un site de partage de pdf accès plutôt sur la high-tech, programmation et tout types de sciences (mais si vous êtes là pour d'autre raisons restez, c'est disponible).</h5>
+      <h4 class="w3-text-grey">฿ibliotecha, premier site de partage de pdf totalement gratuit sans pub.</h4>
+      <h5 class="w3-padding-32">C'est un site de partage de pdf accès plutôt sur la high-tech, programmation et tout types de sciences (mais si vous êtes là pour d'autre raisons restez, c'est disponible).<br />  ฿ibliotecha est un projet qui est née le 29 Décembre 2017, sur le serveur discord Didakt Community.<br>Il a pour but de promouvoir le savoir et la culture, sur le domaine des sciences et de la Hig-Tech grace au partage de livre(PDF), vidéo(MP4,AVI).<br /> D'où son nom ฿ = du BiteCoin, ฿ibli = pour Bibliotéque, tech = pour la Hig-Tech.</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-text-grey"></p>
     </div>
 
     <div class="w3-third w3-center">
@@ -67,18 +74,16 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
     </div>
 
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>Comment ça marche?</h1>
+      <h5 class="w3-padding-32">Les systèmes</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-text-grey">Les documents sont partagés grace à une Data-Base(base de données[Bientôt P2p grace à l'application ^^]), c'est le staf qui gére ces archives, mais c'est la comunauté qui l'alimante en le proposant de manière anonyme ou non sur #Contacte.<br />Ce qui est des pubs, le site s'engage à ne pas mettre de pubs ou de pop-up ou même à vendre vos cookies, le système de rémunération est un Miner intégré à la page web(pas de surchofe de processeur car on ne mais pas la capacité à 100%), le mineur ce charge d'enpreinté votre puissance de calcule pour miner du XMR tout pendant que vous restiez sur ฿ibliotecha.<br />(10 secondes = 0.0001€ ) Cela à été mis en place pour limiter la casse niveau logistique et équipement de ฿ibliotecha; donc si vous pouvez laisser ouvert la page web ^^ .</p>
     </div>
   </div>
 </div>
 
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+    <h1 class="w3-margin w3-xlarge"></h1>
 </div>
 
 <!-- Footer -->
@@ -91,7 +96,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
     <i class="fa fa-twitter w3-hover-opacity"></i>
     <i class="fa fa-linkedin w3-hover-opacity"></i>
  </div>
- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+ <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank"></a>w3css</p>
 </footer>
 
 <script>
