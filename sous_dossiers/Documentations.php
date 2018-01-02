0ef33a7b57a27ba035f80c56b302a85be59c35b4
Poulpitore <Poulpitore@hotmail.com>
2018-01-02 14:29:08 +0100

    0.13

--- sous_dossiers/Documentations.php
+++ sous_dossiers/Documentations.php
@@ -24,7 +24,7 @@
 <div class="w3-top">
   <div class="w3-bar w3-red w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Accueil</a>
+    <a href="../index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Accueil</a>
     <a href="a_propos.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">À propos</a>
     <a href="Nouveautés.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Nouveautées</a>
     <a href="Documentations.php" class="w3-bar-item w3-button w3-padding-large w3-white">Documentations</a>
