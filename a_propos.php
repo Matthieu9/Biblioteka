1494e435be4e6c9caa3bc881f585e6a7bbfad3d8
Matthieu9 <34970602+Matthieu9@users.noreply.github.com>
2018-01-02 15:57:09 +0100

    Modification apporté

--- a_propos.php
+++ a_propos.php
@@ -2,65 +2,20 @@
 <html>
 
 <head>
-<<<<<<< HEAD
   <title>฿IBLIOTECHA - À propos</title>
-=======
-  <title>W3.CSS Template</title>
->>>>>>> Poulpe
+  <style type="text/css">
+    header {
+      background-image: url("include/codecode.jpg");
+      background-repeat: no-repeat;
+      background-size: fill;
+    }
+  </style>
 </head>
-
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<style>
-  body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-  .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-  .fa-address-card-o,.fa-area-chart {font-size:200px}
-</style>
-
-<body>
-
-<!-- Navbar -->
-<div class="w3-top">
-  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-<<<<<<< HEAD
-    <a href="../index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white fa-home">Accueil</a>
-    <a href="a_propos.php" class="w3-bar-item w3-button w3-padding-large w3-white fa-fire">À propos</a>
-    <a href="Nouveautés.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white fa-flask">Nouveautées</a>
-    <a href="Documentations.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white fa-database">Documentations</a>
-    <a href="All_categories.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white fa-tags">Catégories</a>
-    <a href="contact.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white fa-envelope">Contacte</a>
-  </div>
-
-
-
-=======
-    <a href="../index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Accueil</a>
-    <a href="a_propos.php" class="w3-bar-item w3-button w3-padding-large w3-white">À propos</a>
-    <a href="Nouveautés.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Nouveautées</a>
-    <a href="Documentations.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Documentations</a>
-    <a href="All_categories.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Catégories</a>
-    <a href="contact.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contacte</a>
-  </div>
-
->>>>>>> Poulpe
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">À propos</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Nouveautées</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Documentations</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Catégories</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Contacte</a>
-  </div>
-</div>
-
+<?php
+include('include/navbar.php');
+?>
 <!-- Header -->
-<header class="w3-container w3-black w3-center" style="padding:128px 16px">
+<header class="w3-container w3-grey w3-center" style="padding:128px 16px">
   <h1 class="w3-margin w3-text-red w3-jumbo">฿IBLIOTECHA</h1>
   <p class="w3-xlarge w3-text-green">FIND YOUR R00T.</p>
   <button class="w3-button w3-white w3-padding-large w3-large w3-margin-top">RECHERCHER</button>
