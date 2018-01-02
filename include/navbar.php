cdc3be890844849c9ef67dc9f9dd4ce387291dd6
Matthieu9 <34970602+Matthieu9@users.noreply.github.com>
2018-01-02 16:14:20 +0100

    Fini pour moi :) Bonne chance

---
+++ include/navbar.php
@@ -0,0 +1,44 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+  body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+  .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+  .fa-address-card-o,.fa-area-chart {font-size:200px}
+</style>
+
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-white"><i class="fa fa-bars" aria-hidden="true"></i>
+Accueil</a>
+    <a href="a_propos.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-fire" aria-hidden="true"></i>
+À propos</a>
+    <a href="Nouveautés.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-flask" aria-hidden="true"></i>
+Nouveautées</a>
+    <a href="Documentations.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-database" aria-hidden="true"></i>
+Documentations</a>
+    <a href="All_categories.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-tags" aria-hidden="true"></i>
+Catégories</a>
+    <a href="contact.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-envelope" aria-hidden="true"></i>
+Contacte</a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">À propos</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Nouveautées</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Documentations</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Catégories</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Contacte</a>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -3,58 +3,39 @@
 
 <head>
   <title>฿IBLIOTECHA</title>
+    <style type="text/css">
+    header {
+      background-image: url("include/codecode.jpg");
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-attachment: fixed;
+    }
+    #search {
+    	visibility: hidden;
+    }
+  </style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript">
+  	$(document).ready(function(){
+    $("#button").click(function(){
+        $("#search").show();
+    });
+});
+  </script>
 </head>
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="css/font-awesome.min.css">
-
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<style>
-  body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-  .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-  .fa-address-card-o,.fa-area-chart {font-size:200px}
-</style>
-
-<body>
-
-<!-- Navbar -->
-<div class="w3-top">
-  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-white"><i class="fa fa-bars" aria-hidden="true"></i>
-Accueil</a>
-    <a href="sous_dossiers/a_propos.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-fire" aria-hidden="true"></i>
-À propos</a>
-    <a href="sous_dossiers/Nouveautés.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-flask" aria-hidden="true"></i>
-Nouveautées</a>
-    <a href="sous_dossiers/Documentations.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-database" aria-hidden="true"></i>
-Documentations</a>
-    <a href="sous_dossiers/All_categories.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-tags" aria-hidden="true"></i>
-Catégories</a>
-    <a href="sous_dossiers/contact.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"><i class="fa fa-envelope" aria-hidden="true"></i>
-Contacte</a>
-  </div>
-
-  <!-- Navbar on small screens -->
-  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">À propos</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Nouveautées</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Documentations</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Catégories</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Contacte</a>
-  </div>
-</div>
-
+<?php
+include('include/navbar.php');
+?>
 <!-- Header -->
 <header class="w3-container w3-black w3-center" style="padding:128px 16px">
   <h1 class="w3-margin w3-text-red w3-jumbo">฿IBLIOTECHA</h1>
   <p class="w3-xlarge w3-text-green">FIND YOUR R00T.</p>
-  <button class="w3-button w3-white w3-padding-large w3-large w3-margin-top">RECHERCHER</button>
+  <button class="w3-button w3-white w3-padding-large w3-large w3-margin-top" id="button">RECHERCHER</button>
+  <form class="w3-container" id="search">
+<br>
+  <input class="w3-input w3-border" type="text">
+</form>
 </header>
 
 <!-- First Grid -->
